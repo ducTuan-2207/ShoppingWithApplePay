d1ecd792060c0b31f9404da3997f2a737d98e5d5
DucTuan <tuanhypc02@gmail.com>
2024-03-25 23:00:56 +0700

    Initial Commit

---
+++ ShoppingWithApplePay/ShoppingWithApplePayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingWithApplePayApp.swift
+//  ShoppingWithApplePay
+//
+//  Created by macOS on 25/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingWithApplePayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
